0bbf354b386d95cb7d60c31416c44e8ec82fb0af
kuvychko <igor.kuvychko@gmail.com>
2023-10-19 17:08:47 -0700

    implemented Chessboard cmap

--- complexplorer/cmap.py
+++ complexplorer/cmap.py
@@ -13,11 +13,18 @@ def phase(z: complex):
     return phi
 
 class Cmap():
-    def __init__(self, phi_split: Optional[int] = None, r_scale: Optional[float] = None):
-        self.phi_split = phi_split
-        self.r_scale = r_scale
-
+    "Cmap class defines a function that returns a color map corresponding to input complex matrix"
+    def hsv(self, z):
+        raise NotImplementedError(".hsv method not implemented")
+    
+    def rgb(self, z):
+        raise NotImplementedError(".rgb method not implemented")
+    
 class Phase(Cmap):
+    def __init__(self, phi_split: Optional[int] = None, r_log_base: Optional[float] = None):
+        self.phi_split = phi_split
+        self.r_log_base = r_log_base
+
     def hsv(self, z, V_base: float = 0.5):
         if V_base < 0 | V_base > 1:
             raise ValueError("V_base must be within [0, 1) interval")
@@ -29,7 +36,8 @@ class Phase(Cmap):
             V_phi = np.ones_like(z)
         
         if self.r_scale is not None:
-            V_r = np.ceil(np.log(np.abs(z)) * self.r_scale) - np.log(np.abs(z)) * self.r_scale
+            log_z = np.log(np.abs(z)) / np.log(self.r_log_base) # converting log from natural to r_log_base
+            V_r = np.ceil(log_z) - log_z
         else:
             V_r = np.ones_like(z)
         V_scaler = 1 - V_base
@@ -42,3 +50,31 @@ class Phase(Cmap):
         HSV = self.hsv(z, V_base=V_base)
         RGB = colors.hsv_to_rgb(HSV)
         return RGB
+
+class Chessboard(Cmap):
+    def __init__(self, spacing: float = 1., center: complex = 0+0.0j):
+        self.center = center
+        self.spacing = spacing
+
+    def hsv(self, z):
+        S = np.zeros_like(z)
+        H = np.ones_like(z) # fill values do not matter
+        V = np.zeros_like(z) # this defines a white plane
+        z = (z - self.center)/self.spacing # adjusting origin and scaling by spacing
+        real = np.real(z)
+        real_mod = np.mod(real, 2) <= 0
+        real_bool = np.less_equal(real_mod, 1)
+        imag = np.imag(z)
+        imag_mod = np.mod(imag, 2)
+        imag_bool = np.less_equal(imag_mod, 1)
+        V = np.logical_and(real_bool, imag_bool)
+        HSV = np.dstack((H,S,V))
+        return HSV
+    
+    def rgb(self, z):
+        HSV = self.hsv(z)
+        RGB = colors.hsv_to_rgb(HSV)
+        return RGB
+
+class PolarChessboard(Cmap):
+    pass
